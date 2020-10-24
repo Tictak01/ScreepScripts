4e0fb30fbb846e0f6479ca619703a01e3cd16303
Bobby Rowland <rrowland1719@gmail.com>
2020-10-24 09:18:23 -0700

    Upgrader now pulls from containers

--- scratchPad.js
+++ scratchPad.js
@@ -1,1 +1,13 @@
 for(var creep in Game.creeps){console.log(Game.creeps[creep].moveTo(15,33))}
+for(var creep in Game.creeps){console.log(Game.creeps[creep].pos)}
+for(var creep in Game.creeps){console.log(Game.creeps[creep].room.find(FIND_STRUCTURES))}
+
+var roads = creep.room.find(FIND_STRUCTURES, {
+    filter: (structure) => structure.structureType == STRUCTURE_ROAD
+})
+
+Game.creeps['Repairer25941'].pos
+Game.creeps['Repairer25941'].room.find(FIND_STRUCTURES, {filter: (structure) => structure.structureType == STRUCTURE_ROAD})
+
+Game.creeps['Test33836'].claimController(Game.creeps['Test33836'].room.controller)
+Game.creeps['Test33836'].moveTo(Game.creeps['Test33836'].room.controller)

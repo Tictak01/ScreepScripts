6aa44354b83e6756d4f6aa5c6a3efbdd13aa9677
Bobby Rowland <rrowland1719@gmail.com>
2020-10-24 14:56:19 -0700

    More scritches

--- scratchPad.js
+++ scratchPad.js
@@ -11,3 +11,41 @@ Game.creeps['Repairer25941'].room.find(FIND_STRUCTURES, {filter: (structure) => 
 
 Game.creeps['Test33836'].claimController(Game.creeps['Test33836'].room.controller)
 Game.creeps['Test33836'].moveTo(Game.creeps['Test33836'].room.controller)
+
+for(var roomy in Game.rooms){console.log(roomy)}
+
+for(var element in Game.rooms['W2N5']){console.log(element)}
+
+//Gives list of all things at a location.  Terrain appears to be a constant.
+for(var element of Game.rooms['W2N5'].lookAt(13,39)){console.log(element['type'])}
+
+//Check for terrain walls and creeps.  If they equal 9, then the object is surrounded and cannot be accessed.
+
+//Find all elements surrounding the resource
+for(var element of Game.rooms['W2N5'].lookAtArea(38,12,40,14,true)){console.log(element['type'])}
+
+//Count the walls.  Note the source itself also has a wall, so minus one wall.
+var blockerCount = -1;
+for(var element of Game.rooms['W2N5'].lookAtArea(38,12,40,14,true)){
+    if(element['terrain'] == 'wall'){
+        blockerCount += 1;
+        console.log(blockerCount);
+    };
+}
+
+//Count the creeps
+var blockerCount = -1;
+for(var element of Game.rooms['W2N5'].lookAtArea(38,12,40,14,true)){
+    if(element['terrain'] == 'wall'){
+        blockerCount += 1;
+    };
+    if(element['type'] == 'creep'){
+        blockerCount += 1;
+    };
+    console.log(blockerCount);
+}
+
+
+
+//worked
+for(var element of Game.rooms['W2N5'].lookAtArea(38,12,40,14,true)){     console.log(element['type'])     if(element['terrain'] == 'wall'){         console.log('sup')     }; }
